d11fdfe6b31f32fdf7837dfe9b692f8a26c3db76
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-06 12:57:34 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,3 @@
-i was thinking maybe we should create some kind of benchmarking for asr-realtime.
- 
-Could you please research a bit to see what's the state of the art on this
+For latency i am thinking of sending audios of increasing length.. 1 seconds, 2 seconds and so on upto 20 seconds maybe 
+For pauses it will be tricky... Maybe we can synthesise audios with custom pauses
+One more important thing.  We expect to receive speech in realtime mostly via mic. So let's try to use reference audios which is as close to mic as possible (w.r.t. sample rate, file formats etc )

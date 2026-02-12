a4bc8ddaa3f5b73ed21537b7ca24fbee30dd69ee
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-12 08:19:07 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,5 @@
-gcloud alpha ml speech recognizers list \
-  --project=eci-ugi-digital-ccaipoc \
-  --location=us-central1
+docker build \
+  --build-arg USE_PROXY=true \
+  --build-arg HTTP_PROXY=http://163.116.128.80:8080 \
+  --build-arg HTTPS_PROXY=http://163.116.128.80:8080 \
+  -t asr-realtime:latest .

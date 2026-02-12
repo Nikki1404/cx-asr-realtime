4fefc5de8194a16c777425f7508ea0a1200c98bc
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-12 17:31:22 +0530

    Update change.py

--- change.py
+++ change.py
@@ -3,3 +3,47 @@ docker build \
   --build-arg HTTP_PROXY=http://163.116.128.80:8080 \
   --build-arg HTTPS_PROXY=http://163.116.128.80:8080 \
   -t asr-realtime:latest .
+
+A copy of this license is made available in this container at /NGC-DL-CONTAINER-LICENSE for your convenience.
+
+/usr/local/lib/python3.10/dist-packages/torch/cuda/__init__.py:61: FutureWarning: The pynvml package is deprecated. Please install nvidia-ml-py instead. If you did not install pynvml directly, please report this to the maintainers of the package that installed pynvml for you.
+  import pynvml  # type: ignore[import]
+/usr/local/lib/python3.10/dist-packages/transformers/utils/hub.py:110: FutureWarning: Using `TRANSFORMERS_CACHE` is deprecated and will be removed in v5 of Transformers. Use `HF_HOME` instead.
+  warnings.warn(
+Traceback (most recent call last):
+  File "/srv/scripts/run_server.py", line 22, in <module>
+    main()
+  File "/srv/scripts/run_server.py", line 13, in main
+    uvicorn.run(
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/main.py", line 594, in run
+    server.run()
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/server.py", line 67, in run
+    return asyncio_run(self.serve(sockets=sockets), loop_factory=self.config.get_loop_factory())
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/_compat.py", line 60, in asyncio_run
+    return loop.run_until_complete(main)
+  File "uvloop/loop.pyx", line 1518, in uvloop.loop.Loop.run_until_complete
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/server.py", line 71, in serve
+    await self._serve(sockets)
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/server.py", line 78, in _serve
+    config.load()
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/config.py", line 439, in load
+    self.loaded_app = import_from_string(self.app)
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/importer.py", line 22, in import_from_string
+    raise exc from None
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/importer.py", line 19, in import_from_string
+    module = importlib.import_module(module_str)
+  File "/usr/lib/python3.10/importlib/__init__.py", line 126, in import_module
+    return _bootstrap._gcd_import(name[level:], package, level)
+  File "<frozen importlib._bootstrap>", line 1050, in _gcd_import
+  File "<frozen importlib._bootstrap>", line 1027, in _find_and_load
+  File "<frozen importlib._bootstrap>", line 1006, in _find_and_load_unlocked
+  File "<frozen importlib._bootstrap>", line 688, in _load_unlocked
+  File "<frozen importlib._bootstrap_external>", line 883, in exec_module
+  File "<frozen importlib._bootstrap>", line 241, in _call_with_frames_removed
+  File "/srv/app/main.py", line 13, in <module>
+    from app.factory import build_engine
+  File "/srv/app/factory.py", line 4, in <module>
+    from app.asr_engines.google_streaming_asr import GoogleStreamingASR
+  File "/srv/app/asr_engines/google_streaming_asr.py", line 9, in <module>
+    from google.api_core.client_options import ClientOptions
+ModuleNotFoundError: No module named 'google.api_core'

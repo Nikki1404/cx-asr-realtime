5409241a77827770086e4a7ffc5136fec3efb4af
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-12 08:01:59 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,28 +1,8 @@
-(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/bu-digital-cx-asr-realtime# gcloud alpha ml speech recognizers create google-stt-default
---project="eci-ugi-digital-ccaipoc"
---location="us-central1"
---language-codes="en-US"
---model=latest_short
---enable-automatic-punctuation
---enable-word-confidence
---enable-word-time-offsets
-ERROR: (gcloud.alpha.ml.speech.recognizers.create) argument --language-codes --model: Must be specified.
-Usage: gcloud alpha ml speech recognizers create (RECOGNIZER : --location=LOCATION) --language-codes=[LANGUAGE_CODE,...] --model=MODEL [optional flags]
-  optional flags may be  --async | --audio-channel-count | --display-name |
-                         --enable-automatic-punctuation |
-                         --enable-spoken-emojis | --enable-spoken-punctuation |
-                         --enable-word-confidence | --enable-word-time-offsets |
-                         --encoding | --help | --location | --max-alternatives |
-                         --max-speaker-count | --min-speaker-count |
-                         --profanity-filter | --sample-rate |
-                         --separate-channel-recognition
-
-For detailed information on this command and its flags, run:
-  gcloud alpha ml speech recognizers create --help
---project=eci-ugi-digital-ccaipoc: command not found
---location=us-central1: command not found
---language-codes=en-US: command not found
---model=latest_short: command not found
---enable-automatic-punctuation: command not found
---enable-word-confidence: command not found
---enable-word-time-offsets: command not found
+gcloud alpha ml speech recognizers create google-stt-default \
+  --project=eci-ugi-digital-ccaipoc \
+  --location=us-central1 \
+  --language-codes=en-US \
+  --model=latest_short \
+  --enable-automatic-punctuation \
+  --enable-word-confidence \
+  --enable-word-time-offsets

2775e1eeeb9147e6adbc25c60ba1e9abd06bb4a3
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-12 20:35:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 INFO:asr_server:🔥 Using cached google engine (0ms latency!)
-INFO:asr_server:WS connected (google) Address(host='172.17.0.1', port=33972)
+INFO:asr_server:WS connected (google) Address(host='172.17.0.1', port=58766)
 ERROR:grpc._channel:Exception iterating requests!
 Traceback (most recent call last):
   File "/usr/local/lib/python3.10/dist-packages/grpc/_channel.py", line 276, in consume_request_iterator
     request = next(request_iterator)
-  File "/srv/app/asr_engines/google_streaming_asr.py", line 154, in _request_gen
-    streaming_config = cloud_speech.StreamingRecognitionConfig(
+  File "/srv/app/asr_engines/google_streaming_asr.py", line 151, in _request_gen
+    streaming_features=cloud_speech.StreamingRecognitionFeatures(
   File "/usr/local/lib/python3.10/dist-packages/proto/message.py", line 673, in __init__
     raise ValueError(
-ValueError: Unknown field for StreamingRecognitionConfig: interim_results
-INFO:asr_server:WS disconnected
-INFO:     connection closed
+ValueError: Unknown field for StreamingRecognitionFeatures: enable_partial_results

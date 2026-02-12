77de772eaedad67f0476e7393ced45bd3dfed95b
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-02-12 17:51:30 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,8 +1,4 @@
-docker build \
-  --build-arg USE_PROXY=true \
-  --build-arg HTTP_PROXY=http://163.116.128.80:8080 \
-  --build-arg HTTPS_PROXY=http://163.116.128.80:8080 \
-  -t asr-realtime:latest .
+docker build --build-arg USE_PROXY=true --build-arg HTTP_PROXY=http://163.116.128.80:8080 --build-arg HTTPS_PROXY=http://163.116.128.80:8080 -t bu_digital_asr_realtime
 
 A copy of this license is made available in this container at /NGC-DL-CONTAINER-LICENSE for your convenience.
 
